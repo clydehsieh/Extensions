5014fd7173cda18eec3a14e8430e5ab8650bda8d
clydehsieh <clydehsieh0601@gmail.com>
2022-05-14 17:33:51 +0800

    feat: add publisher ext.

---
+++ Publisher+Extension.swift
@@ -0,0 +1,40 @@
+//
+//  Publisher+Extension.swift
+//  Extensions
+//
+//  Created by ClydeHsieh on 2022/5/14.
+//
+import Combine
+
+extension Publisher {
+
+//    The flatMapLatest operator behaves much like the standard FlatMap operator, except that whenever
+//    a new item is emitted by the source Publisher, it will unsubscribe to and stop mirroring the Publisher
+//    that was generated from the previously-emitted item, and begin only mirroring the current one.
+    func flatMapLatest<T: Publisher>(_ transform: @escaping (Self.Output) -> T) -> Publishers.SwitchToLatest<T, Publishers.Map<Self, T>> where T.Failure == Self.Failure {
+        map(transform).switchToLatest()
+    }
+}
+
+extension Publisher {
+
+    static func empty() -> AnyPublisher<Output, Failure> {
+        return Empty().eraseToAnyPublisher()
+    }
+
+    static func just(_ output: Output) -> AnyPublisher<Output, Failure> {
+        return Just(output)
+            .catch { _ in AnyPublisher<Output, Failure>.empty() }
+            .eraseToAnyPublisher()
+    }
+
+    static func fail(_ error: Failure) -> AnyPublisher<Output, Failure> {
+        return Fail(error: error).eraseToAnyPublisher()
+    }
+}
+
+extension Publisher {
+    func unwrap<T>() -> Publishers.CompactMap<Self, T> where Output == Optional<T> {
+        compactMap { $0 }
+    }
+}
